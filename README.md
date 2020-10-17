f07d12b36f2ec9a38ee68f29cff21f1d1137e6de
misaov1 <misaov1@gmail.com>
2020-10-17 11:50:11 -0500

    Updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pywords_eraser_PDF
-Primer Proyecto
+
+Español:
+Este pequeño proyecto fue creado con la finalidad de borrar o censurar palabras dentro de un documento PDF escaneado,
+utilizando tesseract OCR, para la deteccion de palabras y CV2 para marcarlas en el documento.
+
+## Descripcion
+
+probando
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pdf2image import convert_from_path
 from pytesseract import Output
-import cv2,pytesseract,argparse,numpy,os
+import cv2, pytesseract, argparse, numpy, os
+
 
 def file_path(filepath):
     '''
@@ -8,11 +9,14 @@ def file_path(filepath):
     returns: if filepath does exists, it returns the full path of the file.
     '''
     if filepath[-3:] not in 'pdf':
-        raise argparse.ArgumentTypeError(f"Extension:{filepath[-3:]} is not a valid pdf file")
+        raise argparse.ArgumentTypeError(
+            f"Extension:{filepath[-3:]} is not a valid pdf file")
     if os.path.isfile(filepath):
         return filepath
     else:
-        raise argparse.ArgumentTypeError(f"readable_dir:{filepath} is not a valid path")
+        raise argparse.ArgumentTypeError(
+            f"readable_dir:{filepath} is not a valid path")
+
 
 def folder_path(folderpath):
     '''
@@ -22,7 +26,8 @@ def folder_path(folderpath):
     if os.path.isdir(folderpath):
         return folderpath
     else:
-        raise argparse.ArgumentTypeError(f"readable_dir:{folderpath} is not a valid path")
+        raise argparse.ArgumentTypeError(
+            f"readable_dir:{folderpath} is not a valid path")
 
 
 def draw_rectangle(img, pos):
@@ -30,33 +35,38 @@ def draw_rectangle(img, pos):
     This function takes an image, and draws a rectangle on the given coordinates.
     returns: an image file with censored words.
     '''
-    (x,y,w,h) = pos
+    (x, y, w, h) = pos
     return cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 0), -1)
 
 
 def data_ret(pdf_page):
     '''
     This function takes a pdf page.
-    returns: a tupple of a PIL image Object and the text extraction for the given pdf.
+    returns: a tuple of a PIL image Object and the text extraction for the given pdf.
     '''
-    open_cv_image = numpy.array(pdf_page) 
+    open_cv_image = numpy.array(pdf_page)
     return (img := open_cv_image[:, :, ::-1].copy(), pytesseract.image_to_data(img, output_type=Output.DICT))
 
 
 def word_coords(ocr, index):
+    '''
+    returns: a coordinate tuple for the given index word.
+    '''
     return (ocr['left'][index], ocr['top'][index], ocr['width'][index], ocr['height'][index])
 
 
 def logic(path, names, makedir, DPI, matchsensitivity):
     pages = convert_from_path(path, DPI)
+    # path & filename formating
     dir = path.split('\\')
     filename = dir[-1]
     mkdir = '\\'.join(dir[:-1])+f'\\{filename[:-4]}_results'
     dir = '\\'.join(dir[:-1])
 
-    for n,page in enumerate(pages):
+    for n, page in enumerate(pages):
         img, ocr = data_ret(page)
-        word_groups, text, reliability = len(ocr['text']), ocr['text'], ocr['conf']
+        word_groups, text, reliability = len(
+            ocr['text']), ocr['text'], ocr['conf']
 
         for i in range(word_groups):
             for name in names:
@@ -66,38 +76,45 @@ def logic(path, names, makedir, DPI, matchsensitivity):
         if makedir:
             if not os.path.isdir(mkdir):
                 os.makedirs(mkdir)
-            cv2.imwrite(f'{mkdir}\\{filename[:-4]}_Sheet{n+1}_processed.png', img)
-            print(f'{filename[:-4]}_Sheet{n+1}_processed.png, has been created at {mkdir}')
+            cv2.imwrite(
+                f'{mkdir}\\{filename[:-4]}_Sheet{n+1}_processed.png', img)
+            print(
+                f'{filename[:-4]}_Sheet{n+1}_processed.png, has been created at {mkdir}')
         else:
-            cv2.imwrite(f'{dir}\\{filename[:-4]}_Sheet{n+1}_processed.png', img)
-            print(f'{filename[:-4]}_Sheet{n+1}_processed.png, has been created at {dir}')
-        
+            cv2.imwrite(
+                f'{dir}\\{filename[:-4]}_Sheet{n+1}_processed.png', img)
+            print(
+                f'{filename[:-4]}_Sheet{n+1}_processed.png, has been created at {dir}')
 
-def Main(path, names, makedir, folderpath, DPI=30, matchsensitivity = 50):
+
+def Main(path, names, makedir, folderpath, DPI=30, matchsensitivity=50):
+    # If user chose the folderpath argument it will iterate over all pdf files within the folder.
     if folderpath:
         for pdf in os.listdir(folderpath):
             if pdf[-3:] in 'pdf':
-                logic(folderpath+'\\'+pdf, names, makedir, DPI, matchsensitivity)
+                logic(folderpath+'\\'+pdf, names,
+                      makedir, DPI, matchsensitivity)
     else:
         logic(path, names, makedir, DPI, matchsensitivity)
 
 
 if __name__ == "__main__":
-    #Command Line arguments:
+    # Command Line arguments:
     parser = argparse.ArgumentParser(description='Command line arguments.')
     group = parser.add_mutually_exclusive_group()
-    #name is the default argument, this arg takes one or more names, ej. John Doe / John / John Snow Second
-    parser.add_argument('name', nargs='+', help='Type multiple names separated by spaces, these are to be removed from the pdf.')
-    #The document path argument takes a full path including the pdf document itself, if this argument is casted then the folderpath argument will be disabled.
-    group.add_argument('-d','--documentpath', help='This option allows the user to directly point to a pdf document.', type=file_path)
-    #The folder path argument takes a full path for a folder containing one or multiple pdf documents, if this argument is casted then the document argument will be disabled.
-    group.add_argument('-f','--folderpath', help='This option allows the user to set a path folder to multiple pdf files at once.', type=folder_path)
-    #makedir is an argument which takes no value, if this argument is casted then the script will create a folder for each file to place the results into.
-    parser.add_argument('-mk','--makedir', help='This option creates a subfolder (results) under the current file path.', action="store_true")
+    # name is the default argument, this arg takes one or more names, ej. John Doe / John / John Snow Second
+    parser.add_argument(
+        'name', nargs='+', help='Type multiple names separated by spaces, these are to be removed from the pdf.')
+    # The document path argument takes a full path including the pdf document itself.
+    group.add_argument('-d', '--documentpath',
+                       help='This option allows the user to directly point to a pdf document.', type=file_path)
+    # The folder path argument takes a full path for a folder containing one or multiple pdf documents.
+    group.add_argument('-f', '--folderpath',
+                       help='This option allows the user to set a path folder to multiple pdf files at once.', type=folder_path)
+    # makedir is an argument which takes no value, if this argument is casted then the script will create a folder for each file to place the results into.
+    parser.add_argument(
+        '-mk', '--makedir', help='This option creates a subfolder (results) under the current file path.', action="store_true")
     args = parser.parse_args()
-    
-    #Initializating Main function
+
+    # Initializating Main function
     Main(args.documentpath, args.name, args.makedir, args.folderpath)
-
-
-
