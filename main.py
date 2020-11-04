6ae536dae5e04f0cf79a27facc42b752c0140711
misaov1 <misaov1@gmail.com>
2020-11-04 05:23:55 -0600

    agregadas func

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from pdf2image import convert_from_path
 from pytesseract import Output
-import cv2, pytesseract, argparse, numpy, os
+import cv2, pytesseract, argparse, numpy, os, re
+
+
+def txt_path(filepath):
+    '''
+    This function checks if a txt document exists.
+    returns: if filepath does exists, it returns the full path of the file.
+    '''
+    if filepath[-3:] not in 'txt':
+        raise argparse.ArgumentTypeError(
+            f"Extension:{filepath[-3:]} is not a valid txt file")
+    if os.path.isfile(filepath):
+        return filepath
+    else:
+        raise argparse.ArgumentTypeError(
+            f"readable_dir:{filepath} is not a valid path")
 
 
 def check_positive(value):
@@ -55,14 +70,14 @@ def data_ret(pdf_page):
     return (img := open_cv_image[:, :, ::-1].copy(), pytesseract.image_to_data(img, lang="spa", output_type=Output.DICT))
 
 
-def word_coords(ocr, index):
+def word_coords(ocr, index, index2):
     '''
     returns: a coordinate tuple for the given index word.
     '''
-    return (ocr['left'][index], ocr['top'][index], ocr['width'][index], ocr['height'][index])
+    return (ocr['left'][index], ocr['top'][index], (ocr['left'][index2]-ocr['left'][index])+ocr['width'][index2], ocr['height'][index2])
 
 
-def logic(path, names, makedir, DPI, matchsensitivity):
+def logic(path, names, makedir, txtfile, DPI, matchsensitivity):
     pages = convert_from_path(path, DPI)
     # path & filename formating
     dir = path.split('\\')
@@ -70,16 +85,34 @@ def logic(path, names, makedir, DPI, matchsensitivity):
     mkdir = '\\'.join(dir[:-1])+f'\\{filename[:-4]}_results'
     dir = '\\'.join(dir[:-1])
 
+    if txtfile is None and names is None:
+        try:
+            with open (rf'{dir}\\{filename[:-4]}.txt', 'r', encoding='utf-8') as file:
+                names = file.readlines()
+            names = [name.replace('\n', '') for name in names]
+        except:
+            raise Exception(rf"File {filename[:-4]}.txt, doesn't exists")
+
+    elif txtfile is not None and names is None:
+        try:
+            with open (txtfile, 'r', encoding='utf-8') as file:
+                names = file.readlines()
+            names = [name.replace('\n', '') for name in names]
+        except:
+            raise Exception(rf"An error has been found with the current file at {txtfile}.")      
+    else:
+        names = [name.strip() for name in ' '.join(names).split(',') if name != '']
+
     for n, page in enumerate(pages):
         img, ocr = data_ret(page)
         word_groups, text, reliability = len(
             ocr['text']), ocr['text'], ocr['conf']
-
         for i in range(word_groups):
             for name in names:
-                if name.lower() in text[i].lower() and int(reliability[i]) > matchsensitivity:
-                    coords = word_coords(ocr, i)
-                    img = draw_rectangle(img, coords)
+                if re.search(name.split()[0], text[i], re.IGNORECASE) is not None and int(reliability[i]) > matchsensitivity:
+                    if re.search(name, ' '.join(text[i:i+len(name.split())]), re.IGNORECASE) is not None:
+                        img = draw_rectangle(img, word_coords(ocr, i, i+len(name.split())-1))
+    
         if makedir:
             if not os.path.isdir(mkdir):
                 os.makedirs(mkdir)
@@ -94,24 +127,28 @@ def logic(path, names, makedir, DPI, matchsensitivity):
                 f'{filename[:-4]}_Sheet{n+1}_processed.png, has been created at {dir}')
 
 
-def Main(path, names, makedir, folderpath, DPI=200, matchsensitivity=50):
+def Main(path, names, makedir, folderpath, txtfile, DPI=200, matchsensitivity=50):
     # If user chose the folderpath argument it will iterate over all pdf files within the folder.
     if folderpath:
         for pdf in os.listdir(folderpath):
             if pdf[-3:] in 'pdf':
                 logic(folderpath+'\\'+pdf, names,
-                      makedir, DPI, matchsensitivity)
+                      makedir, txtfile, DPI, matchsensitivity)
     else:
-        logic(path, names, makedir, DPI, matchsensitivity)
+        logic(path, names, makedir, txtfile, DPI, matchsensitivity)
 
 
 if __name__ == "__main__":
     # Command Line arguments:
     parser = argparse.ArgumentParser(description='Command line arguments.')
-    group = parser.add_mutually_exclusive_group()
+
+    names = parser.add_mutually_exclusive_group()
     # name is the default argument, this arg takes one or more names, ej. John Doe / John / John Snow Second
-    parser.add_argument(
-        'name', nargs='+', help='Type multiple names separated by spaces, these are to be removed from the pdf.')
+    names.add_argument(
+        '-n', '--name', nargs='+', help='Type multiple names separated by spaces, these are to be removed from the pdf.')
+    names.add_argument(
+        '-txt', '--txtfile', nargs='?', help='Load a text file containing multiple names, these are to be removed from the pdf.', type=txt_path)
+    group = parser.add_mutually_exclusive_group()
     # The document path argument takes a full path including the pdf document itself.
     group.add_argument('-d', '--documentpath',
                        help='This option allows the user to directly point to a pdf document.', type=file_path)
@@ -126,11 +163,10 @@ if __name__ == "__main__":
         '-dpi', help='This option takes an integer value, to set the DPI of the given pdf.', type=check_positive)        
 
     args = parser.parse_args()
-
     
+
     # Initializating Main function
     if args.dpi is not None:
-        print(args.dpi)
-        Main(args.documentpath, args.name, args.makedir, args.folderpath, args.dpi)
+        Main(args.documentpath, args.name, args.makedir, args.folderpath, args.txtfile, args.dpi)
     else:
-        Main(args.documentpath, args.name, args.makedir, args.folderpath)
+        Main(args.documentpath, args.name, args.makedir, args.folderpath, args.txtfile)
